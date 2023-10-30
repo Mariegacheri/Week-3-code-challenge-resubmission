80f7e21e967e598fe394f7acb4bd469a48fa0735
marie <mariegacheri@gmail.com>
2023-10-30 09:44:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# FLATDANGO MOVIE TICKET BOOKING APP
+Welcome to Flatdango, the ultimate solution for booking movie tickets at the Flatiron Movie Theater. This repository contains all the information you need to get started with this project.
+
+# Introduction
+Flatdango is a JavaScript-based application that allows users to purchase movie tickets at the Flatiron Movie Theater. The app fetches movie data from a local server running a JSON DB server and provides a user-friendly interface for selecting and buying tickets.
+
+Before you start coding, it's important to plan your app thoroughly. This README will guide you through the setup and implementation of Flatdango.
+
+Requirements
+For this project, you must meet the following requirements:
+
+Have a well-written README file.
+Fetch data from a local server running a JSON DB server.
+Pre-requisite Data
+You can use this JSON file for your server DB: db.json.
+
+# Project Setup
+Follow these steps to set up your project:
+
+Create a new project folder.
+Create a new private GitHub repository for your project. Ensure it's private to protect your code.
+Add your instructor/TA as a contributor to the project (for grading purposes).
+Regularly commit your code to the repository.
+Project Guidelines
+Your project should conform to the following set of guidelines:
+
+# Installation
+Clone the repository:
+
+git clone git@github.com:Mariegacheri/Week-3-code-challenge-resubmission.git
+Change to the project directory:
+
+cd user-input-json-database
+Core Deliverables
+As a user, you must be able to:
+
+See the first movie's details, including its poster, title, runtime, showtime, and available tickets when the page loads. The number of available tickets will be derived by subtracting the number of tickets_sold from the theater's capacity. You will need to make a GET request to the following endpoint to retrieve the film data:
+GET /films/1
+See a menu of all movies on the left side of the page in the ul#films element when the page loads. (Optional: you can style each film in the list by adding the classes film item to each li element.) You will need to make a GET request to the following endpoint to retrieve the film data:
+GET /films
+Buy a ticket for a movie. After clicking the "Buy Ticket" button, you should see the number of available tickets decreasing on the frontend. You should not be able to buy a ticket if the showing is sold out (if there are 0 tickets available). No persistence is needed for this feature.
+Bonus Deliverables
+These bonus deliverables are here if you want an extra challenge:
+
+Click on a movie in the menu to replace the currently displayed movie's details with the new movie's details. Note that you may have to make an additional GET request to access the movie's details.
+
+When a movie is sold out (when there are no available tickets remaining), indicate that the movie is sold out by changing the button text to "Sold Out." Also update the film item in the ul#films menu by adding a class of sold-out to the film.
+
+Extra Bonus
+These extra bonus deliverables involve using fetch to update data on the json-server backend by using POST, PATCH, and DELETE requests. These are meant for an extra challenge and won't affect your grade.
+
+When a ticket is purchased, persist the updated number of tickets_sold on the server. The frontend shows the number of available tickets based on the tickets_sold and the capacity, so only the tickets_sold should be updated on the backend when a ticket is purchased. You will need to make a request that follows this structure:
+
+Delete a film from the server. Add a delete button next to each film in the ul#films menu. When the button is clicked, remove the film from the list and also delete the film on the server.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Author
+Mary Mwangi,
---
+++ script.js
@@ -0,0 +1,144 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    //Default movie
+    let card;
+    //Movies card
+    let cardDiv;
+    // Show default movie on page load
+    function getDefaultMovie(){
+        const moiveDetails= document.querySelector("#movieDetails")
+        fetch("http://localhost:3000/films")
+        .then(res=> res.json())
+        .then(data => {
+           card = document.createElement("div")
+           moiveDetails.appendChild(card)
+           card.innerHTML = `
+           <div class="card" style="width: 30rem; padding-left: 20px; padding-top: 20px">
+           <img class="card-img-top" style="width: 20rem; padding-left: 20px" src="${data[0].poster}" alt="Card image cap">
+            <div id="defaultCard" class="card-body">
+                <h4 class="card-title">Movie Title: ${data[0].title}</h4>
+                <p class="card-text">${data[0].description}</p>
+                <p class="card-text">Runtime: ${data[0].runtime}</p>
+                <p class="card-text">Showtime: ${data[0].showtime}</p>
+                <p class="card-text">Available Tickets: ${data[0].capacity - data[0].tickets_sold}</p>
+            </div>
+            </div>
+            `
+            const btnDefault = document.createElement("button")
+            btnDefault.className= 'btn btn-primary'
+            
+            let ticketAvailable = data[0].capacity - data[0].tickets_sold;
+            const cardBody= document.querySelector("#defaultCard")
+            //Update button for the first id
+            if(ticketAvailable >0)
+            {
+                btnDefault.textContent = "Buy Ticket"
+                btnDefault.addEventListener("click", (e)=> {
+                    e.preventDefault()
+                    fetch (`http://localhost:3000/films/${data[0].id}`, {
+                    method: 'PATCH',
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                    body:JSON.stringify({tickets_sold: data[0].tickets_sold+1})
+                })
+                .then(res=> res.json())
+                .then((updatedData) => {
+                        // Update the displayed available tickets count
+                        const ticketsAvailableElement = document.getElementById("ticketsAvailable");
+                        ticketsAvailableElement.textContent = `Available Tickets: ${data[0].capacity - updatedData.tickets_sold}`;
+                    })
+                .catch(error=>error)
+                })
+                
+            }
+            else{
+                btnDefault.textContent = "Tickets Sold Out"
+                btnDefault.addEventListener("click", ()=> alert("These tickets are sold out"))
+                btnDefault.removeEventListener("click", null);
+            }
+                cardBody.appendChild(btnDefault)
+        })
+        .catch(error=>error)
+     }
+     getDefaultMovie()
+    // Fetch data and put it on a list on the side nav
+    function getMovieList(){
+        const ul= document.querySelector("#movieMenu")
+        const movieDetails= document.querySelector("#movieDetails")
+        fetch("http://localhost:3000/films")
+        .then(res=> res.json())
+        .then(data => {
+            data.forEach(data => {
+               
+                const listDiv = document.createElement("div")
+                listDiv.className = "film_items"
+                ul.appendChild(listDiv)
+                const list= document.createElement("li")
+                list.className = 'listAtt'
+                list.textContent = data.title
+                list.addEventListener("click", ()=>{
+                    card.style.display = 'none'; 
+                    const existingCard = movieDetails.querySelector(".card-div");
+                    if (existingCard) {
+                        movieDetails.removeChild(existingCard);//remove existing card before adding elements
+                    }
+                    //create card to hold movies onclick
+                        cardDiv = document.createElement("div")
+                        cardDiv.className = "card-div"
+                        cardDiv.innerHTML= `
+                        <div id = "moviesCard" class="card" style="width: 30rem; padding-left: 20px; padding-top: 20px">
+                        <img class="card-img-top" style="width: 20rem; padding-left: 20px" src="${data.poster}" alt="${data.title}">
+                        <div id = "movieCard" class="card-body">
+                            <h4 class="card-title">Movie Title: ${data.title}</h4>
+                            <p class="card-text">${data.description}</p>
+                            <p class="card-text">Runtime: ${data.runtime}</p>
+                            <p class="card-text">Showtime: ${data.showtime}</p>
+                            <p id = "ticketsAvailable" class="card-text">Available Tickets: ${data.capacity - data.tickets_sold}</p>
+                        </div>
+                        </div>
+                        `
+                        movieDetails.appendChild(cardDiv)
+                        //Create buying button and change to sold out if all tickets are not available
+                        const btnBuy = document.createElement("button")
+                        btnBuy.className= 'btn btn-primary'
+                        btnBuy.textContent = "Buy Ticket"
+                        let ticketAvailable = data.capacity - data.tickets_sold;
+                        const cardBody= document.querySelector("#movieCard")
+                        if(ticketAvailable >0)
+                        {
+                            btnBuy.addEventListener("click", (e)=> {
+                                e.preventDefault()
+                                // function to patchh tickets update
+                                fetch (`http://localhost:3000/films/${data.id}`, {
+                                    method: 'PATCH',
+                                    headers:{
+                                        'Content-Type':'application/json'
+                                    },
+                                    body:JSON.stringify({tickets_sold: data.tickets_sold+1})
+                                })
+                                .then(res=> res.json())
+                                .then((updatedData) => {
+                                    // Update the displayed available tickets count
+                                    const ticketsAvailableElement = document.getElementById("ticketsAvailable");
+                                    ticketsAvailableElement.textContent = `Available Tickets: ${data.capacity - updatedData.tickets_sold}`;
+                                  })
+                                .catch(error=>error)
+                                })
+                        }
+                        else{// button and list menu shows sold out if the tickets are sold out
+                            list.textContent= "Tickets Sold Out"
+                            btnBuy.textContent = "Tickets Sold Out"
+                            btnBuy.addEventListener("click", ()=> alert("These tickets are sold out"))
+                            btnBuy.removeEventListener("click", null);
+                        }
+                        
+                        cardBody.appendChild(btnBuy)                   
+                });
+                listDiv.appendChild(list)
+            });
+        })
+        .catch(error=>error)
+    }
+    getMovieList()
+})
